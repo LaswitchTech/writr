e2751be0730058d8d619d3f0b284bcab48ac8bbe
hanzlik <julian@hanzlik.at>
2026-02-07 17:16:52 +0100

    Add German translations

---
+++ lang/de/settings.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Language File for Configuration
+ *
+ * @author   Louis Ouellet <louis_ouellet@hotmail.com>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ */
+
+$lang['_tools']                             = 'Werkzeuge konfigurieren';
+$lang['showPageToolsTitle']                 = 'Titel der Seitenwerkzeuge anzeigen';
+$lang['showSiteToolsTitle']                 = 'Titel der Website-Werkzeuge anzeigen';
+$lang['showUserToolsTitle']                 = 'Titel der Benutzerwerkzeuge anzeigen';
+$lang['doSiteToolsRequireLogin']            = 'Sollen die Website-Werkzeuge eine Anmeldung erfordern';
+
+$lang['_extra']                             = 'Extras konfigurieren';
+$lang['useToolbar']                         = 'Symbolleiste aktivieren';
+$lang['useTooltips']                        = 'Tooltips aktivieren';
+$lang['makeFullWidth']                      = 'Inhaltsbereich auf volle Breite setzen';
+$lang['doLogoChangesByNamespace']           = 'Sollen Logo-Änderungen nach Namespace erfolgen';
+$lang['doLogoLinkChangesByNamespace']       = 'Sollen Änderungen des Logo-Links nach Namespace erfolgen';
+$lang['doTitleChangesByNamespace']          = 'Sollen Änderungen des Seitentitels nach Namespace erfolgen';
+$lang['doTaglineChangesByNamespace']        = 'Sollen Änderungen des Slogans nach Namespace erfolgen';
+
+$lang['_plugins']                           = 'Plugins konfigurieren';
+$lang['defaultAddNewPage']                  = 'Namespace für das AddNewPage-Plugin';
+$lang['enableStarredBookmark']              = 'Markierte Lesezeichen in der Symbolleiste aktivieren';
+
+$lang['_customizations']                    = 'Template anpassen';
+$lang['profile_pic_variant']                = 'Farbe der Umrandung für das Profilbild (falls kein Gravatar vorhanden ist)';
+$lang['profile_pic_variant_o_white']        = 'Weiß';
+$lang['profile_pic_variant_o_black']        = 'Schwarz';
